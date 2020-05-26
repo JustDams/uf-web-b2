e9cd3949c6585c2c86e05af2635f4293b25671a8
Victor Pineau <55828145+Laezare@users.noreply.github.com>
2020-05-26 14:42:51 +0200

    index route

--- src/Controller/GamesController.php
+++ src/Controller/GamesController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class GamesController extends AbstractController
 {
     /**
-     * @Route("/games", name="games")
+     * @Route("/", name="index")
      */
     public function index()
     {
